3fde4142d1149213bbd6c95f68f7d6cf4b0d53de
Omid2831 <mehrabiomid8282@gmail.com>
2026-03-07 14:47:14 +0100

    feat: Add API endpoints for users, properties, rental agreements, favorites, and notifications

---
+++ client/lib/api/endpoints.dart
@@ -0,0 +1,14 @@
+// ignore: dangling_library_doc_comments, slash_for_doc_comments
+/**
+ * API endpoints used in the application
+ * This file contains the API endpoints for the application.
+ * These endpoints will be used to make API requests to the server.
+ */
+
+class Endpoints {
+  static const String users = "/users";
+  static const String properties = "/properties";
+  static const String rentalAgreements = "/rental-agreements";
+  static const String favorites = "/favorites";
+  static const String notifications = "/notifications";
+}
